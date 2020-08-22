db988d322f49bebabbb3cfd6c56efe80dad62dd4
sohta02 <sohta02@fa2.so-net.ne.jp>
2020-08-22 14:01:36 +0900

    add facemesh

--- src/extension-support/extension-manager.js
+++ src/extension-support/extension-manager.js
@@ -17,6 +17,7 @@ const builtinExtensions = {
     M5CameraCar: () => require('../extensions/scratch3_tukurutch/M5CameraCar.js'),
     M5Series: () => require('../extensions/scratch3_tukurutch/M5Series.js'),
     QuadCrawlerEsp: () => require('../extensions/scratch3_tukurutch/QuadCrawlerEsp.js'),
+    facemesh2scratch: () => require('../extensions/scratch3_facemesh2scratch'),
     pen: () => require('../extensions/scratch3_pen'),
     wedo2: () => require('../extensions/scratch3_wedo2'),
     music: () => require('../extensions/scratch3_music'),
